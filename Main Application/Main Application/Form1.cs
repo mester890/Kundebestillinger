b24e9d141c74ed51300a9283abc63a6604e2323e
Thomas <thomas.hovstad@outlook.com>
2016-07-21 00:03:45 +0200

    Added function for appending to access database file. Working perfectly!

--- Main Application/Main Application/Form1.cs
+++ Main Application/Main Application/Form1.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using Excel = Microsoft.Office.Interop.Excel;
-
 
 namespace Main_Application
 {
@@ -14,23 +21,35 @@ namespace Main_Application
 
         private void button1_Click(object sender, EventArgs e)
         {
-            appendData();
+            System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection();
+            conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;" +
+            @"Data source= C:\Users\Public\Documents\test.accdb";
+
+            try
+            {
+                conn.Open();
+                String ticketno = textBox1.Text.ToString();
+                String Purchaseprice = textBox2.Text.ToString();
+                String my_querry = "INSERT INTO test(Navn,Kontakt)VALUES('" + ticketno + "','" + Purchaseprice + "')";
+
+                OleDbCommand cmd = new OleDbCommand(my_querry, conn);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Data saved successfuly...!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed due to" + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
         private void appendData()
         {
 
-            Excel.Application excel = new Excel.Application();
-            excel.Visible = true;
-            Excel.Workbook wb = excel.Workbooks.Open("C:\\test.xlsx");
-            Excel.Worksheet sh = wb.Sheets.Add();
-            sh.Name = "test";
-            sh.Cells[1, "A"].Value2 = "SNO";
-            sh.Cells[2, "B"].Value2 = "A";
-            sh.Cells[2, "C"].Value2 = "1122";
-            wb.Close(true);
-            excel.Quit();
         }
-        
     }
 }
